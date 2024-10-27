ffb788d619adb1e08f1cbe9d7ced4ed074f72b61
57U <uu57uu@yandex.com>
2024-10-27 19:23:38 +0800

    enhance language

--- lib/weather_ui/change_language.dart
+++ lib/weather_ui/change_language.dart
@@ -1,9 +1,11 @@
-
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shape_weather/weather_ui/widgets/controls.dart';
 
 import '../setting/configuration.dart';
+
 class ChangeLanguagePage extends StatefulWidget {
   const ChangeLanguagePage({super.key});
 
@@ -12,38 +14,62 @@ class ChangeLanguagePage extends StatefulWidget {
 }
 
 class _ChangeLanguagePageState extends State<ChangeLanguagePage> {
-  late List<Locale> locales=AppLocalizations.supportedLocales;
+  late List<Locale> locales = AppLocalizations.supportedLocales;
   String? selected;
+
   @override
   void initState() {
-    selected=preferences.getString(language_set);
+    selected = preferences.getString(language_set);
     super.initState();
   }
-  void onValueChanged(String? v){
+
+  void onValueChanged(String? v) {
     setState(() {
-      selected=v;
+      selected = v;
     });
-    appSetting.value[language_set]=selected;
-    appSetting.notifyListeners();
+    appSetting.value[language_set] = selected;
+    Future.delayed(const Duration(milliseconds: 00)).then((onValue) {
+      //ensure the animation finished
+      appSetting.notifyListeners();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var children=<Widget>[];
+    var children = <Widget>[];
     children.add(Padding(
       padding: const EdgeInsets.all(8.0),
-      child: RadioButton(index: null, selected: selected, onChanged: onValueChanged, text: AppLocalizations.of(context)!.followSystem,),
+      child: FollowSystemRadioButton(
+        index: null,
+        selected: selected,
+        onChanged: onValueChanged,
+        text: AppLocalizations.of(context)!.followSystem,
+      ),
     ));
-    for(var i in locales){
+    for (var i in locales) {
       children.add(
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: RadioButton(index: i.toString(), selected: selected, onChanged: onValueChanged, text: i.toLanguageTag(),),
+          child: Localizations.override(
+            context: context,
+            locale: i,
+            child: Builder(builder: (context) {
+              return RadioButton(
+                index: i.toString(),
+                selected: selected,
+                onChanged: onValueChanged,
+                //text: i.toLanguageTag(),
+                text: AppLocalizations.of(context)!.selfLanguage,
+              );
+            }),
+          ),
         ),
       );
     }
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.modifyLanguage),),
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.modifyLanguage),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
@@ -56,28 +82,93 @@ class _ChangeLanguagePageState extends State<ChangeLanguagePage> {
     );
   }
 }
+
 class RadioButton extends StatelessWidget {
   final String? selected;
   final String? index;
   final String text;
   final void Function(String? v) onChanged;
-  const RadioButton({super.key,required this.selected, required this.index, required this.text, required this.onChanged,});
+
+  const RadioButton({
+    super.key,
+    required this.selected,
+    required this.index,
+    required this.text,
+    required this.onChanged,
+  });
+
+  onPress() {
+    onChanged(index);
+  }
 
   @override
   Widget build(BuildContext context) {
-    onPress(){onChanged(index);}
-    Widget child=Row(
+    var selfSelected = selected == index;
+    Widget child = Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text(text,textScaler: const TextScaler.linear(1.1),),
+          child: Text(
+            text,
+            textScaler: const TextScaler.linear(1.1),
+          ),
         ),
-        AnimatedOpacity(opacity: selected==index?1:0, duration: const Duration(milliseconds: 150),child: const Icon(Icons.check),)
+        AnimatedOpacity(
+          opacity: selfSelected ? 1 : 0,
+          duration: const Duration(milliseconds: 150),
+          child: const Icon(Icons.check),
+        )
       ],
     );
-      return OutlinedButton(onPressed: onPress, child: child);
 
+    return OutlinedButton(onPressed: onPress, child: child);
   }
 }
 
+class FollowSystemRadioButton extends RadioButton {
+  const FollowSystemRadioButton(
+      {super.key,
+      required super.selected,
+      required super.index,
+      required super.text,
+      required super.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    var selfSelected = selected == index;
+    Widget child = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          key: ValueKey(text),
+          text,
+          textScaler: const TextScaler.linear(1.1),
+        ),
+        AnimatedOpacity(
+          opacity: selfSelected ? 1 : 0,
+          duration: const Duration(milliseconds: 150),
+          child: const Icon(Icons.check),
+        )
+      ],
+    );
+    var appLang=AppLocalizations.of(context)!;
+    return OutlinedButton(
+        onPressed: onPress,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
+          child: AnimatedSize(
+              duration: cardSizeAnimationDuration,
+              curve: Curves.easeOutQuart,
+              child: selfSelected
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        child,
+                        Text("${appLang.current}: ${appLang.selfLanguage}")
+                      ],
+                    )
+                  : child),
+        ));
+  }
+}
